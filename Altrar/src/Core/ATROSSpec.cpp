c9ccc4657288569a0adb09cf8f433aca37c8f91a
ARessegetes_Stery <46016708+ARessegetesStery@users.noreply.github.com>
2024-08-13 16:41:59 -0400

    fix: further isolate OS-specific macros

--- Altrar/src/Core/ATROSSpec.cpp
+++ Altrar/src/Core/ATROSSpec.cpp
@@ -8,12 +8,17 @@ namespace ATR
 {
 
 #if defined _WIN32
+
+    namespace ATR_WINAPI
+    {
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
+    }
 
 	void OS_ChangeConsoleColor(unsigned int color)
     {
-        HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
-        SetConsoleTextAttribute(hConsole, color);
+        ATR_WINAPI::HANDLE hConsole = ATR_WINAPI::GetStdHandle(-11);            // -11 = STD_OUTPUT_HANDLE
+        ATR_WINAPI::SetConsoleTextAttribute(hConsole, color);
     }
 
 #endif
